8f55d39a271ea5d86defbff1ef2e6d3b9752e04d
Starlyn Fernandez <starlyn_fernandez@wws.com.do>
2024-09-26 10:46:49 -0400

    I had started my base project for express ts

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: 'ts-jest',
+  testEnvironment: "jest-environment-node",
+};
+
+export default config;
---
+++ src/app.test.ts
@@ -0,0 +1,5 @@
+describe('Test App.ts', () => {
+    test('should be true', () => {
+     expect(true).toBeTruthy();
+    });
+   });
